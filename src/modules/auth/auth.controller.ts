4138357afd59469ee9c9490bd69f54ae72ed08f8
CondeArmand <neoarmandosousa1010@hotmail.com>
2025-06-11 20:20:41 -0300

    Update auth.controller.ts

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpCode,
+  HttpStatus,
+  Headers,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 
@@ -8,7 +15,17 @@ export class AuthController {
 
   @Post('register')
   @HttpCode(HttpStatus.CREATED)
-  async register(@Body() registerUserDto: RegisterUserDto) {
+  async register(
+    @Body() registerUserDto: RegisterUserDto,
+    @Headers('Authorization') authHeader: string,
+  ) {
+    // LOG PARA DEPURAÇÃO
+    console.log('============================================');
+    console.log('HEADERS RECEBIDOS DO CLIENTE:');
+    console.log('Raw Authorization Header:', authHeader);
+    console.log('DTO Body (idToken dentro dele):', registerUserDto.idToken);
+    console.log('============================================');
+
     return this.authService.registerUser(registerUserDto);
   }
 }
