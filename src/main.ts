b7680038e10bbd373a06e48dafce73c6d3983649
CondeArmand <neoarmandosousa1010@hotmail.com>
2025-06-20 19:55:41 -0300

    Integrate Redis for session management and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,34 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import session from 'express-session';
 import passport from 'passport';
+import { RedisStore } from 'connect-redis';
+import Redis from 'ioredis';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const redisClient = new Redis({
+    host: process.env.REDIS_HOST,
+    port: parseInt(process.env.REDIS_PORT ?? '123', 10),
+  });
+
+  // 4. Inicializa o RedisStore com o cliente
+  const redisStore = new RedisStore({
+    client: redisClient,
+    prefix: 'gamemate_session:', // Um prefixo para organizar as chaves no Redis
+  });
+
+  // 5. Usa o RedisStore na configuração da sessão
   app.use(
     session({
+      store: redisStore,
       secret: process.env.SESSION_SECRET ?? 'default,secret',
       resave: false,
       saveUninitialized: false,
       cookie: {
-        maxAge: 24 * 60 * 60 * 1000, // 1 dia
-        httpOnly: true, // Protege contra XSS
-        secure: process.env.NODE_ENV === 'production', // Usa cookies seguros em produção
+        secure: process.env.NODE_ENV === 'production',
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7, // Sessão dura 7 dias, por exemplo
       },
     }),
   );
