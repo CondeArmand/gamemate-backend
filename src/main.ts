92d3be9e0d56f9218d9d62acb5d7d45603107fb3
CondeArmand <neoarmandosousa1010@hotmail.com>
2025-06-24 20:08:18 -0300

    Enhance application bootstrap: specify NestExpressApplication type, configure trust proxy, and improve CORS settings

--- src/main.ts
+++ src/main.ts
@@ -40,7 +40,6 @@ async function bootstrap() {
   app.use(passport.session());
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL ?? 'http://localhost:5173',
     credentials: true,
   });
 
